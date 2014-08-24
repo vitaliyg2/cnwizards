49ab9d5dcab57f314ea73c5b2770dc530612add7
vitaliyg2 <vitaliygrabchuk@gmail.com>
2014-08-24 15:25:25 +0300

    Implemented option for selecting current token highlight delay

--- Source/IdeEnhancements/CnSourceHighlightFrm.pas
+++ Source/IdeEnhancements/CnSourceHighlightFrm.pas
@@ -115,6 +115,8 @@ type
     chkCompDirective: TCheckBox;
     shpCompDirective: TShape;
     chkShowLinePosAtGutter: TCheckBox;
+    seCurrentTokenDelay: TCnSpinEdit;
+    lblCurrentTokenDelay: TLabel;
     procedure UpdateControls(Sender: TObject);
     procedure btnHelpClick(Sender: TObject);
     procedure shpBracketMouseDown(Sender: TObject; Button: TMouseButton;
@@ -188,6 +190,7 @@ begin
     rgMatchRange.ItemIndex := Integer(Wizard.BlockHighlightRange);
     rgMatchDelay.ItemIndex := Integer(Wizard.BlockHighlightStyle);
     seDelay.Value := Wizard.BlockMatchDelay;
+    seCurrentTokenDelay.Value := Wizard.CurrentTokenDelay;
     chkMaxSize.Checked := Wizard.BlockMatchLineLimit;
     seMaxLines.Value := Wizard.BlockMatchMaxLines;
     hkMatchHotkey.HotKey := Wizard.BlockMatchHotkey;
@@ -232,6 +235,8 @@ begin
       Wizard.BlockHighlightRange := TBlockHighlightRange(rgMatchRange.ItemIndex);
       Wizard.BlockHighlightStyle := TBlockHighlightStyle(rgMatchDelay.ItemIndex);
       Wizard.BlockMatchDelay := seDelay.Value;
+      Wizard.CurrentTokenDelay := seCurrentTokenDelay.Value;
+
       Wizard.BlockMatchLineLimit := chkMaxSize.Checked;
       Wizard.BlockMatchMaxLines := seMaxLines.Value;
       Wizard.BlockMatchHotkey := hkMatchHotkey.HotKey;
@@ -277,6 +282,8 @@ begin
   lblCurTokenBd.Enabled := chkCurrentToken.Checked;
   shpCurTokenBd.Enabled := chkCurrentToken.Checked;
   chkShowLinePosAtGutter.Enabled := chkCurrentToken.Checked;
+  lblCurrentTokenDelay.Enabled := chkCurrentToken.Checked;
+  seCurrentTokenDelay.Enabled := chkCurrentToken.Checked;
 
   shpCurLine.Enabled := chkHighlightCurLine.Checked;
   shpFlowControl.Enabled := chkFlowControl.Checked;
